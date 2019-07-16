95a90ee5d87823f68d3809d91335ebf1b106fe88
martin <durcansky.martin@gmail.com>
2019-07-16 11:39:56 +0200

    triedenie aut

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;  // Import the Collections class
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Martin!");
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        Collections.sort(cars);  // Sort cars
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
     }
-}
+} 
